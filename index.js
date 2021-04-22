88d2a9b01b4a7ebdc7429a9557cbc2a6ce580313
mustafasheikh1 <mustafaahmed726@gmail.com>
2021-04-22 15:55:58 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+      <div className="container-fluid">
+        <h2>This is a ReactJS App</h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
